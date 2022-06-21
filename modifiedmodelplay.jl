b6cc7f9f70869da540ed851a3beb0a41c331ded7
sandrinewayou <sandrine.wayou@aims-cameroon.org>
2022-06-21 11:25:02 +0200

    add parameter

--- modifiedmodelplay.jl
+++ modifiedmodelplay.jl
@@ -17,30 +17,30 @@ infect_period = [5,10]
 #parameters
 
 par = (
-      α_1 =
-      α_2 =
-      p = 0.5
-      η = 0.5
-      σ_1 =
-      σ_2 =
+      α_1 = 1/50 + 1/15,
+      α_2 = (1/50 + 1/15)*2,
+      p = 0.5,
+      η = 0.5,
+      σ_1 = 1/5,
+      σ_2 = 1/5 * 2
       f_dead1 = 0.03
       f_dead2 = 0.02
-      ϕ =
-      β =
-      μ =
+      ϕ = 1/200,
+      β = 1/500,
+      μ = 1/1000
       contact_par = [t_end,a,r_0,infect_period,ind]
 )
 
 
 #initial condition
-x0 = [sum(ind),0,1,0,0,0]
+x0 = [sum(ind),0,0,1,0,0,0,0,0,0,0]
 t = 0:200
 
 
 num_events = 19
 num_types = 11
 
-hist = zeros(Int,(length(t),6))
+hist = zeros(Int,(length(t),11))
 
 Gillespie.run_gillespie!(
         t,x0,par,
